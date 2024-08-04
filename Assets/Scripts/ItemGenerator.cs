dfe1458434baaec0f9f9f21d9bdbd338c53db738
SrJuanfri <juanfran.ragui@gmail.com>
2024-08-04 20:28:53 +0200

    Periódico y retoques

--- Assets/Scripts/ItemGenerator.cs
+++ Assets/Scripts/ItemGenerator.cs
@@ -8,19 +8,7 @@ public class ItemGenerator : Interactable
     [SerializeField] private Registradora registradora;
     [SerializeField] private ItemSO itemSO;
     [SerializeField] private Transform itemSpawnPoint;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void CreateItem()
     {
--- Assets/Scripts/Player/PlayerPickUpDrop.cs
+++ Assets/Scripts/Player/PlayerPickUpDrop.cs
@@ -7,9 +7,12 @@ public class PlayerPickUpDrop : MonoBehaviour
 {
     [SerializeField] private Transform playerCameraTransform;
     [SerializeField] private Transform objectGrabPointTransform;
+    public Transform readPoint;
     [SerializeField] private LayerMask pickUpLayerMask;
 
     private ObjectGrabbable objectGrabbable;
+    private Newspaper newspaper;
+    private float pickUpDistance = 2f;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.E))
@@ -17,7 +20,7 @@ public class PlayerPickUpDrop : MonoBehaviour
             if(!objectGrabbable)
             {
                 //No llevamos objeto e intentamos cogerlo
-                float pickUpDistance = 2f;
+                
                 if (Physics.Raycast(playerCameraTransform.transform.position, playerCameraTransform.forward, out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
                 {
                     if (raycastHit.transform.TryGetComponent(out objectGrabbable))
@@ -33,6 +36,27 @@ public class PlayerPickUpDrop : MonoBehaviour
                 objectGrabbable = null;
             }
             
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!newspaper)
+            {
+                if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward,
+                        out RaycastHit raycastHit, pickUpDistance, pickUpLayerMask))
+                {
+                    if (raycastHit.transform.TryGetComponent(out newspaper))
+                    {
+                        Debug.Log("Coger Periodico");
+                        newspaper.Read(readPoint);
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Soltar Periodico");
+                newspaper.Drop();
+                newspaper = null;
+            }
         }
     }
 }
