92025b3cb3ab4a8c61fc2ba319cb3c92ce163400
SrJuanfri <juanfran.ragui@gmail.com>
2024-10-10 00:51:39 +0200

    SFX objetos

---
+++ Assets/Scripts/Sound/CollisionSound.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+
+    public AudioSource audioSource;
+    
+
+    public AudioClip collisionSound;
+
+ 
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (audioSource != null && collisionSound != null)
+        {
+            float collisionForce = collision.relativeVelocity.magnitude;
+            
+            audioSource.PlayOneShot(collisionSound, Mathf.Clamp(collisionForce / 10f, 0.1f, 1.0f));
+        }
+    }
+}
